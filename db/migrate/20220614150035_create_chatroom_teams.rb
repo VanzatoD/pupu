953631b7cd222b6b6adb0a2df4fe62f417b8bc15
SHINDY BABA <shindyhh@gmail.com>
2022-06-14 16:01:12 -0300

    added avatar and fixed name of chats for homepage

--- db/migrate/20220614150035_create_chatroom_teams.rb
+++ db/migrate/20220614150035_create_chatroom_teams.rb
@@ -2,9 +2,7 @@ class CreateChatroomTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :chatroom_teams do |t|
       t.string :name
-      t.references :team, null: false, foreign_key: truerails g model Message content chatroom:references user:references
-
-
+      t.references :team, null: false, foreign_key: true
       t.timestamps
     end
   end
